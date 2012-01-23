785f1ce81f10b7bf9a1dd79b1021dda29e04f7da
Mario Adrian <many.mario@gmx.de>
2012-01-23 23:05:43 +0100

    Initial Commit

---
+++ MALayoutManagerTests/MALayoutManagerTests.h
@@ -0,0 +1,13 @@
+//
+//  MALayoutManagerTests.h
+//  MALayoutManagerTests
+//
+//  Created by Mario Adrian on 23.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MALayoutManagerTests : SenTestCase
+
+@end
---
+++ MALayoutManagerTests/MALayoutManagerTests.m
@@ -0,0 +1,32 @@
+//
+//  MALayoutManagerTests.m
+//  MALayoutManagerTests
+//
+//  Created by Mario Adrian on 23.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MALayoutManagerTests.h"
+
+@implementation MALayoutManagerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MALayoutManagerTests");
+}
+
+@end
